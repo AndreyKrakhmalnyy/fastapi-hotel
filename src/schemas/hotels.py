60e6c31066ee3c824e73a29cf11a7d9cbe6bb6ef
Andrey <kas190697@gmail.com>
2024-09-26 22:40:07 +0300

    rename pydantic schemas.

--- src/schemas/hotels.py
+++ src/schemas/hotels.py
@@ -1,10 +1,10 @@
 from pydantic import BaseModel, Field
 
 
-class HotelPutPost(BaseModel):
+class HotelsPutPost(BaseModel):
     location: str = Field(None, description="Город")
     title: str = Field(None, description="Название отеля")
     
-class HotelPatch(BaseModel):
+class HotelsPatch(BaseModel):
     location: str | None = Field(None, description="Город")
     title: str | None = Field(None, description="Название отеля")
