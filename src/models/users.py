8fb2a8da9ea974d229f90ab2a689be85e84f65d4
Andrey <kas190697@gmail.com>
2024-10-01 17:38:06 +0300

    adding uniqueness parameter for 'email' field and migration with alembic for 'users' model

--- src/models/users.py
+++ src/models/users.py
@@ -1,5 +1,5 @@
 from src.database import Base
-from sqlalchemy import String
+from sqlalchemy import String, UniqueConstraint
 from sqlalchemy.orm import Mapped, mapped_column
 
 
@@ -9,3 +9,5 @@ class UsersOrm(Base):
     id: Mapped[int] = mapped_column(primary_key=True)
     email: Mapped[str] = mapped_column(String(100))
     hashed_password: Mapped[str] = mapped_column(String(100))
+    
+    __table_args__ = (UniqueConstraint('email'),)
