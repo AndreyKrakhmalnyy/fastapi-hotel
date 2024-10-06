8184b6284ce99520466f3f18a65e1d485acd8e20
Andrey <kas190697@gmail.com>
2024-10-06 17:52:23 +0300

    minor changes in imports for src/models/bookings.py

--- src/models/bookings.py
+++ src/models/bookings.py
@@ -1,7 +1,8 @@
 from datetime import date
-from src.database import Base
+from sqlalchemy.ext.hybrid import hybrid_property
 from sqlalchemy import DateTime, ForeignKey, Integer
 from sqlalchemy.orm import Mapped, mapped_column
+from src.database import Base
 
 class BookingsOrm(Base):
     __tablename__ = 'bookings'
