c1544166750d402b163c9de7efd18300caacd563
Andrey <kas190697@gmail.com>
2024-10-09 19:01:16 +0300

    adding endpoints GET /bookings and refactoring endpoint POST /bookings

--- src/api/bookings.py
+++ src/api/bookings.py
@@ -5,14 +5,27 @@ from src.api.dependencies import DBDep, UserIdDep
 router = APIRouter(prefix="/bookings", tags=["Бронирование"])
 
 
-@router.post("/{room_id}")
+@router.get("/all")
+async def get_bookings(db: DBDep):
+    return await db.bookings.get_all()
+    
+    
+@router.get("/me")
+async def get_bookings_of_user(db: DBDep, user_id: UserIdDep):
+    bookings = await db.bookings.get_filtered(user_id=user_id)
+    return {'status': 'OK', 'data': bookings}
+
+@router.post("")
 async def post_bookings(
-    db: DBDep, 
+    db: DBDep,
     user_id: UserIdDep,
-    room_id: int, 
     booking_data: BookingRequestAdd = Body()
 ):
-    _booking_data = BookingAdd(room_id=room_id, user_id=user_id, **booking_data.model_dump())
+    room = await db.rooms.get_one_or_none(id=booking_data.room_id)
+    room_price = room.price
+    _booking_data = BookingAdd(
+    user_id=user_id, price=room_price, **booking_data.model_dump()
+    )
     booking = await db.bookings.add_one(_booking_data)
     await db.commit()
     return {"status": "OK", "data": booking}
