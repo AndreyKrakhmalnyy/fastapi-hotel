dc8e8bb2f9c2a385fcfb707118f6d377ab07af5b
Andrey <kas190697@gmail.com>
2024-10-09 18:45:30 +0300

    refactoring code in src/schemas

--- src/schemas/bookings.py
+++ src/schemas/bookings.py
@@ -1,14 +1,17 @@
-from pydantic import BaseModel, Field
+from pydantic import BaseModel, ConfigDict, Field
 from datetime import date
 
 class BookingRequestAdd(BaseModel):
+    room_id: int
     date_from: date = Field(description='Дата начала бронирования')
     date_to: date = Field(description='Дата конца бронирования')
 
 class BookingAdd(BaseModel):
+    user_id: int
     room_id: int
     date_from: date = Field(description='Дата начала бронирования')
     date_to: date = Field(description='Дата конца бронирования')    
+    price: int = Field(description='Стоимость номера за сутки')
 
 class Booking(BookingAdd):
     id: int
