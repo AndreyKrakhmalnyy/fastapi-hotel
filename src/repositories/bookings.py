09c69120ef5481899e8185a444b7ecd03585752b
Andrey <kas190697@gmail.com>
2024-10-13 17:11:57 +0300

    Добавление аутентификации

--- src/repositories/bookings.py
+++ src/repositories/bookings.py
@@ -3,7 +3,6 @@ from src.schemas.bookings import Booking
 from src.models.bookings import BookingsOrm
 from src.repositories.base import BaseRepository
 
-
 class BookingsRepository(BaseRepository):
     model = BookingsOrm
     schema = Booking
