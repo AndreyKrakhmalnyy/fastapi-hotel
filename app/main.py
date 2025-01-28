8c466edd5ee3f9ddad15826fc299865dd282bdcf
akrakhmalnyi <akrakhmalnyi@astralinux.ru>
2025-01-28 12:43:08 +0300

    chore: raname routers in main.py

--- app/main.py
+++ app/main.py
@@ -6,14 +6,14 @@ from contextlib import asynccontextmanager
 
 from fastapi_cache import FastAPICache
 from fastapi_cache.backends.redis import RedisBackend
-
-from app.api.facilities import router as facility_hotels
-from app.api.bookings import router as router_bookings
-from app.api.rooms import router as router_rooms
-from app.api.auth import router as router_auth
-from app.api.hotels import router as router_hotels
 from app.redis_client import redis_manager
-from app.api.images import router as images_router
+
+from app.api.facilities import router as facility_router
+from app.api.bookings import router as booking_router
+from app.api.rooms import router as room_router
+from app.api.auth import router as auth_router
+from app.api.hotels import router as hotel_router
+from app.api.images import router as image_router
 
 sys.path.append(str(Path(__file__).parent.parent))
 
@@ -35,12 +35,12 @@ async def lifespan(_: FastAPI):
 
 app = FastAPI(root_path="/api", lifespan=lifespan)
 
-app.include_router(router_hotels)
-app.include_router(router_auth)
-app.include_router(router_rooms)
-app.include_router(router_bookings)
-app.include_router(facility_hotels)
-app.include_router(images_router)
+app.include_router(hotel_router)
+app.include_router(auth_router)
+app.include_router(room_router)
+app.include_router(booking_router)
+app.include_router(facility_router)
+app.include_router(image_router)
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
