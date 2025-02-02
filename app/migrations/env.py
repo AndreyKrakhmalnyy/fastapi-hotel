3c30cb07b506ea075e366576c658ecdeef2c03f4
akrakhmalnyi <akrakhmalnyi@astralinux.ru>
2025-02-02 14:09:38 +0300

    refactoring: replaced db models imports from env to init.py (app/models), add init.py to precommit config

--- app/migrations/env.py
+++ app/migrations/env.py
@@ -2,10 +2,9 @@ from alembic import context
 from sqlalchemy import pool
 from sqlalchemy import engine_from_config
 from logging.config import fileConfig
-
 from app.config import settings
 from app.database import Base
-
+from app.models import *  # flake8: ignore
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
---
+++ app/models/__init__.py
@@ -0,0 +1,10 @@
+"""Импорт всех модулей с моделями"""
+
+__all__ = [
+    "bookings",
+    "facilities",
+    "rooms_facilities",
+    "hotels",
+    "rooms",
+    "users",
+]
